85c300d5660d83f3123383f5885eb2cbd4d5932d
parsgit <nvioir@gmail.com>
2022-02-13 15:51:18 +0330

    update

---
+++ app/src/main/java/com/android/opensocket/lib/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.android.opensocket.lib
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
